07d4cf26e4c24b5a420112c5a2d34259b1ff4981
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2021-02-27 23:19:23 -0300

    feat: add components challengebox, countdown end experiencebar

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 const ExperienceBar: React.FC = () => {
+  const { currenteExperience, experienceToNextLevel } = useContext(ChallengesContext);
+
+const percentToNextLevel = Math.round(currenteExperience * 100) / experienceToNextLevel
+
   return (
     <header  className={styles.experienceBar}>
         <span>0 xp</span>
             <div>
-                <div style={{ width: '50%' }} />
-                <span className={styles.currentExperience} style={{ left: '50%' }}> 
-                    300 xp 
+                <div style={{ width: `${percentToNextLevel}%` }} />
+                <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}> 
+                    {currenteExperience} xp 
                 </span>
             </div>
-        <span>600 xp</span>
+        <span>{experienceToNextLevel} xp</span>
     </header>
   );
 }
